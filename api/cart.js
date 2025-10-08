b251184a54866b623883b4efc9c0fbaf261d48e8
Subhan-Tanveer <subhanrider1978@gmail.com>
2025-10-08 14:16:19 -0700

    Add missing @radix-ui/react-tooltip dependency

--- api/cart.js
+++ api/cart.js
@@ -1,6 +1,6 @@
 let cartItems = [];
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
